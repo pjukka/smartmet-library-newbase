a21c1a296746b0594ad42b92510a0e93d4bc9b8f
Marko Pietarinen <marko.pietarinen@fmi.fi>
2017-03-25 10:02:35 +0200

    Had to move NFmiFastInfoUtils from smarttool to newbase. Also had to split one of the functions to own new CtrlViewFastInfoFunctions in ctrlviewutils library.

---
+++ newbase/NFmiFastInfoUtils.cpp
@@ -0,0 +1,87 @@
+#include "NFmiFastInfoUtils.h"
+#include <NFmiFastQueryInfo.h>
+#include <NFmiProducerName.h>
+#include <NFmiFileString.h>
+
+namespace NFmiFastInfoUtils
+{
+bool IsInfoShipTypeData(NFmiFastQueryInfo &theInfo)
+{
+  if (theInfo.IsGrid() == false)
+  {
+    FmiProducerName prodId = static_cast<FmiProducerName>(theInfo.Producer()->GetIdent());
+    if (prodId == kFmiSHIP || prodId == kFmiBUOY)
+      return true;
+  }
+  return false;
+}
+
+// synop-dataa ei tarvitsee asettaa mihink‰‰n, mutta
+// mm. luotaus plotin yhteydess‰ pit‰‰ etsi‰ oikea korkeus/level
+void SetSoundingDataLevel(const NFmiLevel &theWantedSoundingPressureLevel, NFmiFastQueryInfo &info)
+{
+  if (theWantedSoundingPressureLevel.LevelType() == kFmiSoundingLevel)
+  {  // sounding plotin yhteydess‰ pit‰‰ asettaa level haluttuun korkeuteen
+    float levelValue = theWantedSoundingPressureLevel.LevelValue();
+    FmiParameterName parName = static_cast<FmiParameterName>(info.Param().GetParamIdent());
+    unsigned long parIndex = info.ParamIndex();
+    bool subParaUsed = info.IsSubParamUsed();
+    if (info.Param(kFmiPressure))
+    {
+      for (info.ResetLevel(); info.NextLevel();)
+        if (info.FloatValue() == levelValue)
+          break;
+    }
+    if (subParaUsed)
+      info.Param(parName);  // pakko vet‰‰ t‰m‰ hitaalla tavalla jostain syyst‰
+    else
+      info.ParamIndex(parIndex);
+  }
+}
+
+std::string GetTotalDataFilePath(const boost::shared_ptr<NFmiFastQueryInfo> &theInfo)
+{
+  NFmiFileString totalFilePath(theInfo->DataFilePattern());
+  totalFilePath.FileName(theInfo->DataFileName());
+  return std::string(totalFilePath);
+}
+
+bool IsYearLongData(boost::shared_ptr<NFmiFastQueryInfo> &info)
+{
+    const auto &timeDescriptor = info->TimeDescriptor();
+    auto timeDiffInDays = timeDescriptor.LastTime().DifferenceInDays(timeDescriptor.FirstTime());
+    if(timeDiffInDays >= 364 && timeDiffInDays <= 366)
+        return true;
+    else
+        return false;
+}
+
+bool IsModelClimatologyData(boost::shared_ptr<NFmiFastQueryInfo> &info)
+{
+    if(info && info->DataType() == NFmiInfoData::kClimatologyData)
+    {
+        if(info->IsGrid())
+        {
+            if(IsYearLongData(info))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+NFmiMetTime GetUsedTimeIfModelClimatologyData(boost::shared_ptr<NFmiFastQueryInfo> &theInfo, const NFmiMetTime &theTime)
+{
+    if(NFmiFastInfoUtils::IsModelClimatologyData(theInfo))
+    {
+        // For year long climatology data, used time must be fixed to data's own year
+        auto usedTime(theTime);
+        usedTime.SetYear(theInfo->TimeDescriptor().FirstTime().GetYear());
+        return usedTime;
+    }
+    else
+        return theTime;
+}
+
+}
---
+++ newbase/NFmiFastInfoUtils.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "NFmiMetTime.h"
+#include <boost/shared_ptr.hpp>
+
+class NFmiFastQueryInfo;
+class NFmiLevel;
+
+namespace NFmiFastInfoUtils
+{
+bool IsInfoShipTypeData(NFmiFastQueryInfo &theInfo);
+void SetSoundingDataLevel(const NFmiLevel &theWantedSoundingPressureLevel, NFmiFastQueryInfo &info);
+std::string GetTotalDataFilePath(const boost::shared_ptr<NFmiFastQueryInfo> &theInfo);
+bool IsYearLongData(boost::shared_ptr<NFmiFastQueryInfo> &info);
+bool IsModelClimatologyData(boost::shared_ptr<NFmiFastQueryInfo> &info);
+NFmiMetTime GetUsedTimeIfModelClimatologyData(boost::shared_ptr<NFmiFastQueryInfo> &theInfo, const NFmiMetTime &theTime);
+}
