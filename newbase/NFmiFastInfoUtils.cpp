050f64aca060b9f17389aac737a138bcd356c97f
Marko Pietarinen <marko.pietarinen@fmi.fi>
2017-03-30 11:35:14 +0300

    Added three new functions that deal with the moving sounding data. Fixed FindTimeIndicesForGivenTimeRange functionality so that it doesn't return extra hour time-index anymore. Changed code to search starting time forward (not backward).

--- newbase/NFmiFastInfoUtils.cpp
+++ newbase/NFmiFastInfoUtils.cpp
@@ -82,4 +82,96 @@ NFmiMetTime GetUsedTimeIfModelClimatologyData(boost::shared_ptr<NFmiFastQueryInf
   else
     return theTime;
 }
+
+bool IsMovingSoundingData(const boost::shared_ptr<NFmiFastQueryInfo> &theInfo)
+{
+    if(theInfo && !theInfo->IsGrid())
+    {
+        if(theInfo->SizeLevels() > 7)
+            return theInfo->HasLatlonInfoInData();
+    }
+    return false;
+}
+
+bool FindTimeIndicesForGivenTimeRange(const boost::shared_ptr<NFmiFastQueryInfo> &theInfo, const NFmiMetTime &theStartTime, long minuteRange, unsigned long &timeIndex1, unsigned long &timeIndex2)
+{
+    theInfo->FindNearestTime(theStartTime, kForward);
+    timeIndex1 = theInfo->TimeIndex();
+    NFmiMetTime endTime(theStartTime);
+    endTime.ChangeByMinutes(minuteRange);
+    theInfo->FindNearestTime(endTime, kBackward);
+    timeIndex2 = theInfo->TimeIndex();
+
+    if(timeIndex1 == timeIndex2) // pitää testata erikois tapaus, koska TimeToNearestStep-palauttaa aina jotain, jos on dataa
+    {
+        theInfo->TimeIndex(timeIndex1);
+        NFmiMetTime foundTime(theInfo->Time());
+        if(foundTime > endTime || foundTime < theStartTime) // jos löydetty aika on alku ja loppu ajan ulkopuolella ei piirretä salamaa
+            return false;
+    }
+    return true;
+}
+
+bool FindMovingSoundingDataTime(const boost::shared_ptr<NFmiFastQueryInfo> &theInfo,
+    const NFmiMetTime &theTime,
+    NFmiLocation &theLocation)
+{
+    theInfo->FirstLocation();  // liikkuvissa luotauksissa vain yksi dummy paikka, laitetaan se päälle
+    NFmiMetTime timeStart(theTime);
+    timeStart.ChangeByMinutes(-30);
+    unsigned long timeIndex1 = 0;
+    unsigned long timeIndex2 = 0;
+    if(FindTimeIndicesForGivenTimeRange(theInfo, timeStart, 60, timeIndex1, timeIndex2) == false)
+        return false;
+
+    float lat = 0;
+    float lon = 0;
+    theInfo->Param(kFmiLatitude);
+    unsigned long latIndex = theInfo->ParamIndex();
+    theInfo->Param(kFmiLongitude);
+    unsigned long lonIndex = theInfo->ParamIndex();
+    double minDistance = 99999999;
+    unsigned long minDistTimeInd = static_cast<unsigned long>(-1);
+    for(unsigned long i = timeIndex1; i <= timeIndex2; i++)
+    {
+        theInfo->TimeIndex(i);
+
+        for(theInfo->ResetLevel(); theInfo->NextLevel();)
+        {
+            theInfo->ParamIndex(latIndex);
+            lat = theInfo->FloatValue();
+            theInfo->ParamIndex(lonIndex);
+            lon = theInfo->FloatValue();
+
+            if(lat != kFloatMissing && lon != kFloatMissing)
+            {
+                NFmiLocation loc(NFmiPoint(lon, lat));
+                double currDist = theLocation.Distance(loc);
+                if(currDist < minDistance)
+                {
+                    minDistance = currDist;
+                    minDistTimeInd = i;
+                }
+            }
+        }
+    }
+    if(minDistance < 1000 * 1000)  // jos liikkuva luotaus paikka löytyi vähintäin 1000 km säteeltä hiiren
+                                   // klikkauspaikasta, otetaan kyseinen luotaus piirtoon
+    {
+        theInfo->TimeIndex(minDistTimeInd);
+        // pitää lisäksi asettaa locationiksi luotauksen alkupiste
+        theInfo->FirstLevel();
+        theInfo->ParamIndex(latIndex);
+        lat = theInfo->FloatValue();
+        theInfo->ParamIndex(lonIndex);
+        lon = theInfo->FloatValue();
+        theLocation.SetLatitude(lat);
+        theLocation.SetLongitude(lon);
+
+        return true;
+    }
+
+    return false;
+}
+
 }
