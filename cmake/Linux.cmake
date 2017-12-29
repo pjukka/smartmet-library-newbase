b5254010ab6b181def97f0493686b25e381f4c4d
pjukka <pjukka@users.noreply.github.com>
2017-12-29 12:28:06 +0200

    Add CMake option DISABLE_GDAL for linux
    By default GDAL is enabled.

--- cmake/Linux.cmake
+++ cmake/Linux.cmake
@@ -1,4 +1,12 @@
 message(STATUS "System Name: ${CMAKE_SYSTEM_NAME}")
 message(STATUS "Install Prefix: ${CMAKE_INSTALL_PREFIX}")
+
+option(DISABLE_GDAL "Disable GDAL" OFF)
+
 set(CMAKE_CXX_FLAGS "-DUNIX -D_REENTRANT -DFMI_COMPRESSION -DBOOST -DBOOST_IOSTREAMS_NO_LIB")
+
+if(DISABLE_GDAL)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DDISABLED_GDAL")
+endif()
+
 add_compile_options(-std=c++11 -fPIC -MD -Wall -W -Wno-unused-parameter)
