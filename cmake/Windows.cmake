f98130bbc8756d75c66767e6735f1b0cb44b4de6
pjukka <pjukka@users.noreply.github.com>
2018-08-29 13:53:36 +0300

    Move Boost find_package to Windows specific cmake file.

--- cmake/Windows.cmake
+++ cmake/Windows.cmake
@@ -1,1 +1,11 @@
 find_package(Fmt)
+
+if(NOT Boost_INCLUDE_DIRS)
+    set(Boost_USE_STATIC_LIBS ON)
+    set(Boost_USE_MULTITHREADED ON)
+    set(Boost_USE_STATIC_RUNTIME OFF)
+    find_package(Boost 1.60.0 QUIET COMPONENTS regex thread filesystem iostreams system date_time)
+    if (Boost_FOUND AND NOT BOOST_INCLUDE_DIR)
+        set(BOOST_INCLUDE_DIR "${Boost_INCLUDE_DIR}")
+    endif()
+endif()
